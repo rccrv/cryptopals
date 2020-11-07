d6d9d3c9a3cf5bba1e812f9bb0d3cb3a6e54586d
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-07 19:16:24 -0300

    2020-11-07

---
+++ set01/challenge03/cpp/main.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+const tuple<char, double> ALPHABET[26] = {
+    make_tuple('a', 0.08167), make_tuple('b', 0.01492),
+    make_tuple('c', 0.02782), make_tuple('d', 0.04253),
+    make_tuple('e', 0.12702), make_tuple('f', 0.02228),
+    make_tuple('g', 0.02015), make_tuple('h', 0.06094),
+    make_tuple('i', 0.06966), make_tuple('j', 0.00153),
+    make_tuple('k', 0.00772), make_tuple('l', 0.04025),
+    make_tuple('m', 0.02406), make_tuple('n', 0.06749),
+    make_tuple('o', 0.07507), make_tuple('p', 0.01929),
+    make_tuple('q', 0.00095), make_tuple('r', 0.05987),
+    make_tuple('s', 0.06327), make_tuple('t', 0.09056),
+    make_tuple('u', 0.02758), make_tuple('v', 0.00978),
+    make_tuple('w', 0.02360), make_tuple('x', 0.00150),
+    make_tuple('y', 0.01974), make_tuple('z', 0.00074),
+};
+
+double chi_square(string &xored) {
+  double sum = 0.0;
+  uint16_t count[26] = {0};
+
+  for (char c : xored) {
+    if (islower(c)) {
+      count[c - 'a'] += 1;
+    } else if (isdigit(c)) {
+      sum += 5.0;
+    } else if (isspace(c)) {
+      sum += 0.1;
+    } else if (ispunct(c)) {
+      sum += 5.0;
+    } else {
+      return numeric_limits<double>::infinity();
+    }
+  }
+
+  for (int i = 0; i < 26; i++) {
+    auto o = count[i];
+    auto e = get<1>(ALPHABET[i]) * xored.size();
+    sum += (o - e) * (o - e) / e;
+  }
+
+  return sum;
+}
+
+int main() {
+  string s =
+      "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+  string r = "";
+  tuple<char, double> bestfit =
+      make_tuple('\0', numeric_limits<double>::infinity());
+  vector<uint8_t> bytes = {};
+  vector<uint8_t> l = {};
+  for (int i = 65; i < 65 + 26; i++) {
+    l.push_back(i);
+    l.push_back(i + 32);
+  }
+  for (int i = 48; i < 48 + 10; i++) {
+    l.push_back(i);
+  }
+
+  for (int i = 0; i < s.length(); i += 2) {
+    bytes.push_back(stoi("0x" + s.substr(i, 2), NULL, 16));
+  }
+
+  for (char c : l) {
+    auto v = bytes;
+    for_each(v.begin(), v.end(), [c](uint8_t &b) { b ^= c; });
+    string xored = string(v.begin(), v.end());
+    auto lower = xored;
+    transform(lower.begin(), lower.end(), lower.begin(),
+              [](unsigned char c) -> unsigned char { return tolower(c); });
+    auto n = chi_square(lower);
+    if (n < get<1>(bestfit)) {
+      bestfit = make_tuple(c, n);
+      r = xored;
+    }
+  }
+
+  cout << "Decoded by using " << get<0>(bestfit) << "\nResult: " << r << '\n';
+
+  return 0;
+}
---
+++ set01/challenge03/fsharp/Program.fs
@@ -0,0 +1,68 @@
+﻿open System
+
+let ALPHABET =  [|
+    ('a', 0.08167);
+    ('b', 0.01492);
+    ('c', 0.02782);
+    ('d', 0.04253);
+    ('e', 0.12702);
+    ('f', 0.02228);
+    ('g', 0.02015);
+    ('h', 0.06094);
+    ('i', 0.06966);
+    ('j', 0.00153);
+    ('k', 0.00772);
+    ('l', 0.04025);
+    ('m', 0.02406);
+    ('n', 0.06749);
+    ('o', 0.07507);
+    ('p', 0.01929);
+    ('q', 0.00095);
+    ('r', 0.05987);
+    ('s', 0.06327);
+    ('t', 0.09056);
+    ('u', 0.02758);
+    ('v', 0.00978);
+    ('w', 0.02360);
+    ('x', 0.00150);
+    ('y', 0.01974);
+    ('z', 0.00074)
+|]
+
+let chisquare (xored: string) : double =
+    let mutable sum = 0.0
+    let mutable count = Array.init 26 uint16
+    for c in xored do
+        if Char.IsLower(c) then
+            count.[int c - int 'a'] <- count.[int c - int 'a'] + 1us
+        elif Char.IsNumber(c) then
+            sum <- sum + 5.0
+        elif Char.IsWhiteSpace(c) then
+            sum <- sum + 0.1
+        elif Char.IsPunctuation(c) then
+            sum <- sum + 5.0
+        else
+            sum <- Double.PositiveInfinity
+
+    if not <| Double.IsInfinity(sum) then
+        for i in seq { 0 .. 25} do
+            let o = double count.[i]
+            let e = snd ALPHABET.[i]
+            sum <- sum + (o - e) * (o - e) / e
+    sum
+
+[<EntryPoint>]
+let main argv =
+    let s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    let l = [ seq { 'a' .. 'z' }; seq { 'A' .. 'Z' }; seq { '0' .. '9'} ] |> Seq.concat
+    let mutable bestfit = ('\000', Double.PositiveInfinity)
+    let mutable r = ""
+    let bytes = seq { for i in 0 .. 2 .. s.Length - 1 do Convert.ToByte(s.[i..i+1], 16) } |> List.ofSeq
+    for c in l do
+        let xored = String.concat "" <| (List.map ((fun b -> char (b ^^^ byte c)) >> string) bytes)
+        let n = chisquare(xored.ToLower())
+        if n < snd bestfit then
+            bestfit <- (c, n)
+            r <- xored
+    printfn "Decoded by using %c\nResult: %s" (fst bestfit) r
+    0
