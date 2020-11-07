6b2045f6dfa4d0e3547f31dc799807f05d195afa
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-07 16:46:04 -0300

    2020-11-07

--- set01/challenge03/csharp/Program.cs
+++ set01/challenge03/csharp/Program.cs
@@ -2,40 +2,104 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace set01_challenge03
+namespace challenge03
 {
     class Program
     {
-        // TODO: ETAOIN SHRDLU
+        static private (char, double)[] ALPHABET = {
+            ('a', 0.08167),
+            ('b', 0.01492),
+            ('c', 0.02782),
+            ('d', 0.04253),
+            ('e', 0.12702),
+            ('f', 0.02228),
+            ('g', 0.02015),
+            ('h', 0.06094),
+            ('i', 0.06966),
+            ('j', 0.00153),
+            ('k', 0.00772),
+            ('l', 0.04025),
+            ('m', 0.02406),
+            ('n', 0.06749),
+            ('o', 0.07507),
+            ('p', 0.01929),
+            ('q', 0.00095),
+            ('r', 0.05987),
+            ('s', 0.06327),
+            ('t', 0.09056),
+            ('u', 0.02758),
+            ('v', 0.00978),
+            ('w', 0.02360),
+            ('x', 0.00150),
+            ('y', 0.01974),
+            ('z', 0.00074),
+        };
+        static double chi_square(string xored)
+        {
+            double sum = 0.0;
+            ushort[] count = new ushort[26];
+            Array.Clear(count, 0, count.Length);
+
+            foreach (char c in xored)
+            {
+                if (Char.IsLower(c))
+                {
+                    count[(byte)c - (byte)'a'] += 1;
+                }
+                else if (Char.IsNumber(c))
+                {
+                    sum += 5.0;
+                }
+                else if (Char.IsWhiteSpace(c))
+                {
+                    sum += 0.1;
+                }
+                else if (Char.IsPunctuation(c))
+                {
+                    sum += 5.0;
+                }
+                else
+                {
+                    return Double.PositiveInfinity;
+                }
+            }
+
+            foreach (var i in Enumerable.Range(0, 26))
+            {
+                var o = count[i];
+                var e = ALPHABET[i].Item2 * xored.Length;
+                sum += (o - e) * (o - e) / e;
+            }
+
+            return sum;
+        }
         static void Main(string[] args)
         {
-            string s1 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-            Dictionary<char, string> d = new Dictionary<char, string>();
-
-            var l3 = Enumerable.Range(97, 26).ToList();
-            var l2 = Enumerable.Range(65, 26).ToList();
-            l2.AddRange(l3);
+            string s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+            string r = "";
+            List<byte> bytes = new List<byte>();
+            (char, double) bestfit = ('\0', Double.PositiveInfinity);
             var l = Enumerable.Range(48, 10).ToList();
-            l.AddRange(l2);
+            l.AddRange(Enumerable.Range(65, 26).ToList());
+            l.AddRange(Enumerable.Range(97, 26).ToList());
 
-            List<byte> v1 = new List<byte>();
-            for (int i = 0; i < s1.Length; i += 2)
+            for (int i = 0; i < s.Length; i += 2)
             {
-                string sub = s1.Substring(i, 2);
-                byte b = 0;
-
-                b += sub[0] >= '0' && sub[0] <= '9' ? (byte)(sub[0] - '0') : (byte)(sub[0] - 'a' + 10);
-                b <<= 4;
-                b += sub[1] >= '0' && sub[1] <= '9' ? (byte)(sub[1] - '0') : (byte)(sub[1] - 'a' + 10);
-
-                v1.Add(b);
+                bytes.Add(Convert.ToByte(s.Substring(i, 2), 16));
             }
 
-            l.ForEach(k => d.Add((char)k, String.Join("", v1.Select(i => (char)(i ^ k)))));
-            foreach (var v in d)
+            foreach (var k in l)
             {
-                Console.WriteLine(v);
+                string xored = String.Join("", bytes.Select(i => (char)(i ^ k)));
+                var n = chi_square(xored.ToLower());
+                if (n < bestfit.Item2)
+                {
+                    bestfit = ((char)k, n);
+                    r = xored;
+                }
             }
+
+            Console.WriteLine($"Decoded by using {bestfit.Item1}\nResult: {r}");
         }
     }
 }
