8a190ce4ca677941424bd9255ad267bd8142d55a
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-09 19:46:50 -0300

    2020-11-09

---
+++ set01/challenge06/fsharp/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.Numerics
+
+let hd s1 s2 =
+    let lhd = (fun ac c -> ac + BitOperations.PopCount (uint32 ((uint8 (fst c)) ^^^ (uint8 (snd c)))))
+    let sum = Seq.fold lhd 0 (Seq.zip s1 s2)
+    sum
+
+[<EntryPoint>]
+let main argv =
+    let s1 = "this is a test"
+    let s2 = "wokka wokka!!!"
+    let r = hd s1 s2
+    printfn "%d" r
+    0
