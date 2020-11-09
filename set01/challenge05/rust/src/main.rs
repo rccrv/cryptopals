e94c0901b79fe2616aa7248bae33778e9ad5a403
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-09 08:11:02 -0300

    2020-11-09

--- set01/challenge05/rust/src/main.rs
+++ set01/challenge05/rust/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let s = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
-    let v = vec!['I', 'C', 'E'];
-    let mut iter = v.iter().cycle();
+    let key = "ICE";
+    let mut iter = key.chars().cycle();
 
     let bytes = s.as_bytes();
 
     for i in bytes {
         let c = match iter.next() {
-            Some(i) => *i,
+            Some(i) => i,
             None => panic!("Should always receive a character from a cycled iterator")
         };
         print!("{:02x}", i ^ c as u8);
