e67eed355c060b25cac5df4eec4d12c057b49c4d
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-11 14:56:18 -0300

    2020-11-11

--- set01/challenge06/fsharp/Program.fs
+++ set01/challenge06/fsharp/Program.fs
@@ -10,13 +10,16 @@ let hd s1 s2 =
 type Analyze =
     struct
         val content: list<byte>
-        val mutable smallerkeys: list<int>
-        new (fname: string) = {
+        val mutable smallerkeys: array<int>
+        val minsizes: int
+        new (fname: string, size: int) = {
             content = Convert.FromBase64String (File.ReadAllText fname) |> List.ofArray
-            smallerkeys = [0; 0; 0]
+            minsizes = size
+            smallerkeys = Array.zeroCreate size
         }
 
         member this.ProcessKeysize(keysize: int) =
+            let mutable v = []
             let s = [|
                 new string (List.map (char) this.content.[0..(keysize - 1)] |> Array.ofList);
                 new string (List.map (char) this.content.[keysize..(2 * keysize - 1)] |> Array.ofList);
@@ -27,18 +30,50 @@ type Analyze =
                 (0, 1); (0, 2); (0, 3);
                 (1, 2); (1, 3); (2, 3)
             |]
-            let mutable v = []
             for i in indices do
                 v <- v @ [(float (hd s.[fst i] s.[snd i])) / (float keysize)]
             let r = List.sum v / (float indices.Length)
             r
+
+        member this.ProbableKeysizes(range: seq<int>) =
+            let mutable v = []
+            for i in range do
+                v <- v @ [this.ProcessKeysize(i)]
+            if v.Length > 2 then
+                let mins = (List.sort v).[0..3]
+                let r = [|
+                    2 + List.findIndex (fun i -> i = mins.[0]) v;
+                    2 + List.findIndex (fun i -> i = mins.[1]) v;
+                    2 + List.findIndex (fun i -> i = mins.[2]) v;
+                    2 + List.findIndex (fun i -> i = mins.[3]) v;
+                |]
+                Array.Copy(r, this.smallerkeys, this.minsizes)
+            ()
+
+        member this.BreakString() =
+            let mutable v = []
+            for i in this.smallerkeys do
+                let divisions = this.content.Length / i
+                (*
+                for j in seq { 1 .. divisions} do
+                    v <- v @ [ this.content[j..j-1] ]
+                    0 |> ignore
+                *)
+                let lastsize = this.content.Length - i * divisions
+                printfn "%d, %d" divisions lastsize
+            ()
     end
 
 [<EntryPoint>]
 let main argv =
     if argv.Length >= 1 then
-        let analyze = Analyze argv.[0]
-        for i in seq { 2 .. 40 } do
-            let r = analyze.ProcessKeysize i
-            printfn "(%d, %f)" i r
+        let analyze = Analyze(argv.[0], 4)
+        printfn "%d" analyze.content.Length
+        analyze.ProbableKeysizes(seq { 2 .. 40 }) |> ignore
+        printfn "%A" analyze.smallerkeys
+        let firstkey = analyze.smallerkeys.[0]
+        let s = analyze.content.[0..firstkey-1]
+        let news = (Seq.map (char >> string) s) |> String.concat ""
+        printfn "%A"  news
+        analyze.BreakString() |> ignore
     0
