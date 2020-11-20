0a78c15af5b610f8e232668b0f777df09e58ddf0
Ricardo Carvalho <rat.carvalho@gmail.com>
2020-11-20 13:52:54 -0300

    2020-11-20

--- set01/challenge08/Program.fs
+++ set01/challenge08/Program.fs
@@ -1,6 +1,69 @@
 open System
+open System.IO
+
+let bytesentropy (b: list<byte>) =
+    let f =
+        fun (l: list<(bool * int)>) ->
+            let n = (List.filter (fun i -> (fst i)) l)
+
+            let r =
+                if n.Length > 0 then float (snd n.[0]) / float (b.Length) else 0.0
+
+            r
+
+    let s = Set.ofList b
+
+    let prob =
+        seq {
+            for n in s do
+                f (List.countBy (fun i -> i = n) b)
+        }
+        |> List.ofSeq
+
+    let entropy =
+        -Seq.sum
+            (seq {
+                for p in prob do
+                    p * Math.Log2(p)
+             })
+
+    entropy
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello world"
+    // One problem of AES in ECB mode is that when blocks of 16 bytes are equal they will produce
+    // the same output of 16 bytes. I assumed that the line containing the smallest Shannon entropy
+    // would be the right one given the problem statement put emphasis on the previous information.
+    //
+    // The idea is that I just do a simple calculation instead of doing analysis 16 bytes at a time
+    // for each line. If you actually print all the entropies. Line 16 is almost 0.5 larger than
+    // the next smaller value, thus it is our encrypted line.
+    let s2b =
+        fun (s: string) ->
+            seq {
+                for i in 0 .. 2 .. s.Length - 1 do
+                    Convert.ToByte(s.[i..i + 1], 16)
+            }
+            |> List.ofSeq
+
+    if argv.Length >= 1 then
+        let mutable ln = 1
+        let mutable minimumentropy = Double.PositiveInfinity
+        let mutable minimumline = 1
+        let lines = File.ReadAllLines argv.[0]
+
+        for line in lines do
+            let e = (bytesentropy (s2b line))
+
+            if e < minimumentropy then
+                minimumline <- ln
+                minimumentropy <- e
+
+            ln <- ln + 1
+
+        printfn
+            "Line %d has the smallest entroty (%f) and probably was encrypted with AES in ECB mode"
+            minimumline
+            minimumentropy
+
     0
